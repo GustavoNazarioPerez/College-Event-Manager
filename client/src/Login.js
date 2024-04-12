d355c481715ac6062a184b126978d3544f68b2e1
Gustavo Nazario Perez <gustavonazario27@gmail.com>
2024-04-12 16:17:52 -0400

    Session navigation working

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Login.css';
 
@@ -7,7 +7,7 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,28 +20,26 @@ function Login() {
             console.log(response);
             // Check the response data to determine if the login attempt was successful
             if (response.status === 200) {
-                // Redirect to dashboard or home page after successful login
-                window.location.href = '/';
+                localStorage.setItem('userId', response.data.userId); // Store user ID in localStorage
+                navigate('/home'); // Redirect to home page after successful login
             } else {
                 // Handle login error
                 console.error('Login failed');
                 setErrorMessage('Invalid email or password');
-                setSuccessMessage('');
             }
         } catch (error) {
             console.error('Error logging in:', error);
             setErrorMessage('An error occurred while logging in');
-            setSuccessMessage('');
         }
     };
 
     return (
-        <div className='modal-container'> {/* Apply class name for modal container */}
-            <div className='modal'> {/* Apply class name for modal */}
+        <div className='modal-container'>
+            <div className='modal'>
                 <h2>Login</h2>
                 {errorMessage && <p className="error">{errorMessage}</p>}
                 <form onSubmit={handleSubmit}>
-                    <div className='input-group'> {/* Apply class name for input group */}
+                    <div className='input-group'>
                         <label>Email</label>
                         <input
                             type="email"
@@ -50,7 +48,7 @@ function Login() {
                             onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
-                    <div className='input-group'> {/* Apply class name for input group */}
+                    <div className='input-group'>
                         <label>Password</label>
                         <input
                             type="password"
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,33 @@
 // Home.js
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from "./Navbar";
 
 function Home() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Get userId from localStorage
+        const userId = localStorage.getItem('userId');
+        // Log userId to console
+        console.log('User ID:', userId);
+    }, []); // Run this effect only once after the component mounts
+
+    const handleSignOut = () => {
+        // Clear userId from localStorage
+        localStorage.removeItem('userId');
+        // Redirect to login page
+        navigate('/login');
+    };
+
     return (
         <div>
             <h2>Home Page</h2>
+            <div>
+                <Navbar />
+                {/* Sign-out button */}
+                <button onClick={handleSignOut}>Sign Out</button>
+            </div>
             <p>Welcome to the Home page!</p>
         </div>
     );
